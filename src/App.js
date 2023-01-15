b3ca87482f399ba01356fdb4d9e5ce152a665235
angelinaturchyn <angelinaturchyn@gmail.com>
2023-01-15 16:23:57 -0600

    added animation

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {NavBar} from "./components/navBar";
+import {Banner} from "./components/banner"
 
 function App() {
   return (
     <div className="App">
       <NavBar/>
-
+        <Banner/>
     </div>
   );
 }
-
 export default App;
