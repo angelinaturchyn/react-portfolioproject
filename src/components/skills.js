111df4fb3fc49e9fde45439e44524573244bcf8f
angelinaturchyn <angelinaturchyn@gmail.com>
2023-01-17 15:53:39 -0600

    added skills description

--- src/components/skills.js
+++ src/components/skills.js
@@ -1,45 +1,127 @@
-import 'react-multi-carousel/lib/styles.css';
-import Container from "react-bootstrap/Container";
-import {Col, Row} from "react-bootstrap";
-import Carousel from 'react-bootstrap/Carousel';
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import { Container, Row, Col, Tab, Nav, } from "react-bootstrap";
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
 
+
+
+
+const Item = styled(Paper)(({ theme }) => ({
+    backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#2e2e30',
+    ...theme.typography.body2,
+    padding: theme.spacing(2),
+    textAlign: 'left',
+     color: '#eee6f2',
+
+}));
 export const Skills = () => {
-
-    const responsive = {
-        superLargeDesktop: {
-            breakpoint: {max: 4000, min: 3000},
-            items: 5
-        },
-        desktop: {
-            breakpoint: {max: 3000, min: 1024},
-            items: 3
-        },
-        tablet: {
-            breakpoint: {max: 1024, min: 464},
-            items: 2
-        },
-        mobile: {
-            breakpoint: {max: 464, min: 0},
-            items: 1
-        }
-    }
-
         return (
-            <Carousel>
-                <Carousel.Item>
-                    <h1>Skills</h1>
-                    <p>Languages:  Javascript, Java</p>
-                    <p>Database: MongoDB, SQL</p>
-                    <p>API Testing: Postam, REST API, Swagger</p>
-                    <p>Test Approaches: Waterfall, Agile/Scrum, SDLC, STLC</p>
-                    <p>Defect Management Tools: JIRA, TestRail, Shortcut</p>
-                    <p> Smoke, Integration Testing, End- to-End Testing</p>
-                    <p> Technologies: Git, GitHub, HTML, CSS, React.js, Node.js</p>
-                    <p>System and User Acceptance Testing for every modification in the application and new build</p>
-                    <p>Operating Systems:  Apple macOS, Microsoft Windows, Google's Android OS, Kali Linux</p>
-                    <p> Automation Tools: Playwright, Cypress, Appium Mobile automation</p>
-                    <p>Functional GUI, UI, Compatibility, Sanity, Regression, Cross-browser,</p>
-                </Carousel.Item>
-            </Carousel>
+            <Container>
+                <h1>Skills</h1>
+          <Grid container spacing={3}>
+              <Grid item xs={6}>
+                  <Item>
+                              <h5>
+                                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                       className="bi bi-star-fill" viewBox="0 0 16 16">
+                                      <path
+                                          d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
+                                  </svg> Languages:  Javascript, Java
+                              </h5>
+                               <h5>
+                                   <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                        className="bi bi-star-fill" viewBox="0 0 16 16">
+                                       <path
+                                           d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
+                                   </svg>    Database: MongoDB, SQL
+                               </h5>
+                               <h5>
+                                   <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                        className="bi bi-star-fill" viewBox="0 0 16 16">
+                                       <path
+                                           d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
+                                   </svg>   API Testing: Postam, REST API, Swagger
+                               </h5>
+                               <h5>
+                                   <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                        className="bi bi-star-fill" viewBox="0 0 16 16">
+                                       <path
+                                           d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
+                                   </svg>   Test Approaches: Waterfall, Agile/Scrum, SDLC, STLC
+                               </h5>
+                               <h5>
+                                   <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                        className="bi bi-star-fill" viewBox="0 0 16 16">
+                                       <path
+                                           d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
+                                   </svg>   Defect Management Tools: JIRA, TestRail, Shortcut
+                               </h5>
+                               <h5>
+                                   <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                        className="bi bi-star-fill" viewBox="0 0 16 16">
+                                       <path
+                                           d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
+                                   </svg>   Operating Systems:  Apple macOS, Microsoft Windows, Google's Android OS, Kali Linux
+                               </h5>
+                                <h5>
+                                   <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                         className="bi bi-star-fill" viewBox="0 0 16 16">
+                                   <path
+                                  d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
+                                   </svg>   BDD (Behavior Driven Development), TDD (Test Driven Development) using Cucumber in Gherkins format
+                      </h5>
+
+                  </Item>
+              </Grid>
+              <Grid item xs={6}>
+                  <Item>
+                      <h5>
+                          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                               className="bi bi-star-fill" viewBox="0 0 16 16">
+                              <path
+                                  d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
+                          </svg> Testing Approaches: Smoke, Integration Testing, End- to-End Testing
+                      </h5>
+                      <h5>
+                          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                               className="bi bi-star-fill" viewBox="0 0 16 16">
+                              <path
+                                  d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
+                          </svg>    Technologies: Git, GitHub, HTML, CSS, React.js, Node.js
+                      </h5>
+                      <h5>
+                          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                               className="bi bi-star-fill" viewBox="0 0 16 16">
+                              <path
+                                  d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
+                          </svg>   System and User Acceptance Testing for every modification in the application and new build
+                      </h5>
+                      <h5>
+                          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                               className="bi bi-star-fill" viewBox="0 0 16 16">
+                              <path
+                                  d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
+                          </svg>   Automation Tools: Playwright, Cypress, Appium Mobile automation
+                      </h5>
+                      <h5>
+                          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                               className="bi bi-star-fill" viewBox="0 0 16 16">
+                              <path
+                                  d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
+                          </svg>  Functional GUI, UI, Compatibility, Sanity, Regression, Cross-browser
+                      </h5>
+                  </Item>
+              </Grid>
+          </Grid>
+            </Container>
     )
+
 }
+
+// <p> Testing Approaches: Smoke, Integration Testing, End- to-End Testing</p>
+// <p> Technologies: Git, GitHub, HTML, CSS, React.js, Node.js</p>
+// <p>System and User Acceptance Testing for every modification in the application and new build</p>
+// <p> Automation Tools: Playwright, Cypress, Appium Mobile automation</p>
+//BDD (Behavior Driven Development), TDD (Test Driven Development) using Cucumber in Gherkins format.
+// <p>Functional GUI, UI, Compatibility, Sanity, Regression, Cross-browser,</p>
